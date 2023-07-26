e4f7af13f227ee0bc4c51216c631f7b8e242c6a5
Vivan <vivanpanda13@gmail.com>
2023-07-26 15:53:16 +0400

    Create homepage basic contents using javascript

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import './assets/styles/style.css'
-import homeBackgroundImage from './assets/img/home-background.png';
+import { addHomepageToDOM } from "./homepage";
 
-const content = document.querySelector('#content');
-
-const homeBackground = new Image()
-homeBackground.src = homeBackgroundImage;
-homeBackground.classList.add('background-image')
-content.appendChild(homeBackground)
+addHomepageToDOM()
