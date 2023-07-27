bfc6042eba28fba15838380d313a691ec4912b83
Vivan <vivanpanda13@gmail.com>
2023-07-27 09:49:54 +0400

    Create contact page with same page content structure as menu

--- src/contactpage.js
+++ src/contactpage.js
@@ -37,4 +37,22 @@ export function addContactPageToDOM() {
     homeBackground.src = homeBackgroundImage;
     homeBackground.classList.add('background-image')
     content.appendChild(homeBackground)
+
+    const contactContainer = document.createElement('div')
+    contactContainer.classList.add('menu-container')
+    content.appendChild(contactContainer);
+
+    const contactHeader = document.createElement('h1')
+    contactHeader.classList.add('menu-header')
+    contactHeader.innerHTML = 'Contact us!'
+    contactContainer.appendChild(contactHeader)
+
+    const contactContent = document.createElement('div')
+    contactContent.classList.add('menu-content')
+    contactContainer.appendChild(contactContent)
+
+    const contactDescription = document.createElement('p')
+    contactDescription.classList.add('menu-description')
+    contactDescription.innerHTML = 'You can contact us from here.'
+    contactContent.appendChild(contactDescription)
 }
